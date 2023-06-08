d083c1250e57dfcc16d332e4abb7617acfb23c07
Danyl_Sakhno <danjadanjasakhno@gmail.com>
2023-06-08 20:56:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+int factorial( int n) {
+    int result = 1;
+    for ( int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+     int number;
+    cout << "Введіть число: ";
+    cin >> number;
+
+    int result = factorial(number);
+    cout << "Факторіал числа " << number << " дорівнює " << result << endl;
+
+    return 0;
+}
+
+
+
